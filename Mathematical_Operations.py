59a882f94ea673999570085c5ba3e9627c29fe6e
HeatherHanusa <hhanusa@students.mchenry.edu>
2025-08-28 11:30:31 -0500

    Add files via upload
    Mathematical Operations

---
+++ Mathematical_Operations.py
@@ -0,0 +1,16 @@
+#perform basic mathematical operations
+print("Addition:", 5 + 10)
+print("Subtraction:", 9 - 3)
+print("Multiplication:", 2 * 5)
+print("Division:", 12 / 4)
+print("Integer Division:", 13 // 4)
+print("Modulus:", 9 % 4)
+print("Exponentiation:", 2 ** 2)
+
+#The area of a rectangle with a length of 5 and a width of 3 is 15
+Length = 5
+Width = 3
+Area = Length * Width
+print("The area of the rectangle with a length of", Length)
+print("and the width of", Width)
+print("is", Area)
