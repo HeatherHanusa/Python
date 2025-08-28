d2a89d0b357cdd9191dee5e0816e9bd13bdb7ad9
HeatherHanusa <hhanusa@students.mchenry.edu>
2025-08-28 11:02:49 -0500

    Add files via upload
    Data Types

---
+++ Data_Types.py
@@ -0,0 +1,13 @@
+# This is a Python program to demonstrate different data types
+#Literal 
+print("Literal:", 10)
+#Integer 
+print("Integer:", -20)
+#Float 
+print("Float:", 15.5)
+#String 
+name = "John"
+print("String:", name)
+#Boolean
+is_active = True 
+print("Boolean:", is_active)
