bf5e072ad4dd34e98ab810b84c5585dd967a1035
HeatherHanusa <hhanusa@students.mchenry.edu>
2025-09-04 14:09:33 -0500

    Add files via upload
    Python Comments

---
+++ Python_Comments.py
@@ -0,0 +1,20 @@
+"""
+Directions
+-Type the above code into Visual Studio Code.
+- Add a minimum of 4 comments to the above Python program to make it more understandable.
+"""
+tank_size = 15
+# Define the total distance covered
+distance = 600
+# Calculate miles per gallon
+mpg = distance / tank_size
+# Print the result
+print("Miles per gallon:", mpg)
+# This is comment number 1
+print("The tank size is 15 gallons.")
+# This is comment number 2
+print("The distance is 600 miles to cover.")
+# This is comment number 3
+print("The calculation to figure out mpg is distance divided by tank size.")
+# This is comment number 4
+print("The total miles per gallon equals 40.")
