aa57ee4ba469a6e37f9df70ead50f112a8d13be4
HeatherHanusa <hhanusa@students.mchenry.edu>
2025-09-14 14:27:27 -0500

    Add files via upload
    For Loops in Python

---
+++ Loops_in_Python.py
@@ -0,0 +1,6 @@
+"""
+Python Assignment: Find numbers divisible by 7
+Write a python program to find and print all numbers divisible by 7 between 1 and 300. Use a for loop and the modulus operator (%)
+"""
+for i in range(7, 300, 7):
+    print(i)
