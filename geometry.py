5cc227f213cbdf63ca1b65ed75811b9bd1be0891
HeatherHanusa <hhanusa@students.mchenry.edu>
2025-10-08 14:37:48 -0500

    Add files via upload
    PE2 Module 1.3 Packages

---
+++ geometry.py
@@ -0,0 +1,6 @@
+def area_rectangle(length, width):
+    return length * width
+
+def area_circle(radius):
+    from math import pi
+    return pi * radius**2
---
+++ main.py
@@ -0,0 +1,17 @@
+import calculator
+
+result = calculator.add(5, 3)
+print("Addition result:", result)
+
+result = calculator.subtract(10, 4)
+print("Subtraction result:", result)
+
+import calculator
+
+result = calculator.add(7, 2)
+print("Result:", result)
+
+import geometry
+
+print("Rectangle area:", geometry.area_rectangle(6, 10))
+print("Circle area:", geometry.area_circle(7))
